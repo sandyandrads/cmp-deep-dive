6b2b836c35b158e42dc1c4e5241fa70cfd8045c4
Sandy Andrade <sandyandrads@hotmail.com>
2026-01-30 07:37:10 -0300

    more css, binding css, hostbinding and hostlistener

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
+})
+export class ControlComponent {
+  label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+  }
+}
