7e335d84a039bc7f5c9c659f5713768a77163c51
Sandy Andrade <sandyandrads@hotmail.com>
2026-02-01 10:22:23 -0300

    using onDestroy and template variables

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {}
+
+  ngOnInit() {
+    const interval = setInterval(() => {
+      const rnd = Math.random();
+
+      if(rnd < 0.5){
+        this.currentStatus = 'online';
+      }else if(rnd < 0.9){
+        this.currentStatus = 'offline';
+      }else{
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval)
+    })
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+  }
+
 }
